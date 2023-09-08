569f2ae012d6867dfd12f2686bc102f7c48930df
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-08 15:43:32 -0300

    Structure and implementation of bounds.c

--- bounds.c
+++ bounds.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define ARRAY_SIZE 4
-
 struct bound_data {
     bool is_upperbound;
     bool is_lowerbound;
@@ -11,27 +9,71 @@ struct bound_data {
     unsigned int where;
 };
 
-struct bound_data check_bound(int value, int arr[], unsigned int length) {
+// Prints an array
+static void print_array(int arr[], unsigned int length) {
+    printf("Your array is: [");
+    for (unsigned int i = 0; i < length; ++i) {
+        printf("%d", arr[i]);
+        if (i < length - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+// Reads items into an array
+static void items_in_array(int arr[], unsigned int length) {
+    printf("Enter %u items.\n", length);
+    for (unsigned int i = 0; i < length; ++i) {
+        printf("Item %u: ", i);
+        scanf("%d", &arr[i]);
+    }
+    print_array(arr, length);
+}
+
+// Checks if a value is in an array and if it is the upper or lower bound
+static struct bound_data check_bound(int value, int arr[], unsigned int length) {
     struct bound_data res;
-    //
-    // TODO: COMPLETAR
-    //
+    res.is_upperbound = false;
+    res.is_lowerbound = false;
+    res.exists = false;
+
+    for (unsigned int i = 0; i < length; ++i){
+        if (arr[i] == value) {
+            res.exists = true;
+            res.where = i;
+        }else if (arr[i] <= value) res.is_upperbound = true;
+        else if (arr[i] >= value) res.is_lowerbound = true;
+    }
+    
+    printf("\n");
+
     return res;
 }
 
 int main(void) {
-    int a[ARRAY_SIZE] = {0, -1, 9, 4};
-    int value=9;
-    //
-    // TODO: COMPLETAR - Pedir entrada al usuario
-    //
+    int value;
+    unsigned int ARRAY_SIZE;
+    printf("Enter array size: ");
+    scanf("%u", &ARRAY_SIZE);
+
+    int a[ARRAY_SIZE];
+
+    items_in_array(a, ARRAY_SIZE);
+
+    printf("Enter value to check: ");
+    scanf("%d", &value);
+
     struct bound_data result = check_bound(value, a, ARRAY_SIZE);
 
-    // TODO: REEMPLAZAR LO SIGUIENTE LUEGO POR LA SALIDA QUE PIDE EL ENUNCIADO
-    printf("%d", result.is_upperbound); // Imprime 1
-    printf("%d", result.is_lowerbound); // Imprime 0
-    printf("%u", result.exists);        // Imprime 1
-    printf("%u", result.where);         // Imprime 2
+    // Print results
+    if (result.exists && result.is_lowerbound)
+        printf("Value %d is in array and is the minimum. It is in position %d.\n", value, result.where);
+    else if (result.exists && result.is_upperbound)
+        printf("Value %d is in array and is the maximum. It is in position %d.\n", value, result.where);
+    else if (result.is_lowerbound)
+        printf("Value %d is not in array. It is lower bound.\n", value);
+    else if (result.is_upperbound)
+        printf("Value %d is not in array. It is upper bound.\n",value);
+    else printf("Value %d is not in array. It is not upper or lower bound.\n",value);
 
     return EXIT_SUCCESS;
 }
