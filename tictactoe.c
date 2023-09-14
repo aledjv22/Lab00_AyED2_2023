f416ab3de5a852728e76ddb63f68b086337093b6
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-14 02:12:16 -0300

    Generalization of tictactoe

--- tictactoe.c
+++ tictactoe.c
@@ -4,7 +4,9 @@
 
 #include <assert.h>  /* assert() */
 
-#define CELL_MAX (3 * 3 - 1)
+#define N 4
+
+#define CELL_MAX (N * N - 1)
 
 void print_sep(int length) {
     printf("\t ");
@@ -13,31 +15,31 @@ void print_sep(int length) {
 
 }
 
-void print_board(char board[3][3])
+void print_board(char board[N][N])
 {
     int cell = 0;
 
-    print_sep(3);
-    for (int row = 0; row < 3; ++row) {
-        for (int column = 0; column < 3; ++column) {
+    print_sep(N);
+    for (int row = 0; row < N; ++row) {
+        for (int column = 0; column < N; ++column) {
             printf("\t | %d: %c ", cell, board[row][column]);
             ++cell;
         }
         printf("\t | \n");
-        print_sep(3);
+        print_sep(N);
     }
 }
 
 // Function responsible for obtaining the winner of a row.
-char get_winner_in_row (char board[3][3], char winner){
+char get_winner_in_row (char board[N][N], char winner){
     bool aux;
 
-    for (unsigned int row = 0; row < 3; ++row){
+    for (unsigned int row = 0; row < N; ++row){
         aux = true;
-        for (unsigned int colum = 0; colum < 2; ++colum){
+        for (unsigned int colum = 0; colum < N-1; ++colum){
             aux = aux && (board[row][colum] == board[row][colum+1]);
 
-            if (colum == 1 && aux)
+            if (colum == N-2 && aux)
                 if (board[row][colum+1] != '-') winner = board[row][colum+1];
         }
         if (winner != '-') break;
@@ -47,15 +49,15 @@ char get_winner_in_row (char board[3][3], char winner){
 }
 
 // Function responsible for obtaining the winner of a column.
-char get_winner_in_column (char board[3][3], char winner){
+char get_winner_in_column (char board[N][N], char winner){
     bool aux;
 
-    for (unsigned int colum = 0; colum < 3; ++colum){
+    for (unsigned int colum = 0; colum < N; ++colum){
         aux = true;
-        for (unsigned int row = 0; row < 2; ++row){
+        for (unsigned int row = 0; row < N-1; ++row){
             aux = aux && (board[row][colum] == board[row+1][colum]);
 
-            if (row == 1 && aux)
+            if (row == N-2 && aux)
                 if (board[row+1][colum] != '-') winner = board[row+1][colum];
         }
         if(winner != '-') break;
@@ -65,21 +67,21 @@ char get_winner_in_column (char board[3][3], char winner){
 }
 
 // Function responsible for obtaining the winner of a diagonal.
-char get_winner_in_diagonal (char board[3][3], char winner){
+char get_winner_in_diagonal (char board[N][N], char winner){
     bool aux = true;
 
-    for (unsigned int rocol = 0; rocol < 2; ++rocol){
+    for (unsigned int rocol = 0; rocol < N-1; ++rocol){
         aux = aux && (board[rocol][rocol] == board[rocol+1][rocol+1]);
 
-        if (rocol == 1 && aux)
+        if (rocol == N-2 && aux)
             if (board[rocol][rocol] != '-') winner = board[rocol][rocol];
     }
 
     aux = true;
-    for (unsigned int row = 0, colum = 2; row < 2 && colum > 0; ++row,--colum){
+    for (unsigned int row = 0, colum = N-1; row < N-1 && colum > 0; ++row,--colum){
         aux = aux && (board[row][colum] == board[row+1][colum-1]);
         
-        if (row == 1 && aux)
+        if (row == N-2 && aux)
             if (board[row][colum] != '-') winner = board[row][colum];
     }
 
@@ -87,7 +89,7 @@ char get_winner_in_diagonal (char board[3][3], char winner){
 }
 
 // Function responsible for giving the winner if there is.
-char get_winner(char board[3][3])
+char get_winner(char board[N][N])
 {
     char winner = '-';
 
@@ -100,12 +102,12 @@ char get_winner(char board[3][3])
 }
 
 // Function responsible for knowing if there is a free cell.
-bool has_free_cell(char board[3][3])
+bool has_free_cell(char board[N][N])
 {
     bool free_cell = false;
 
-    for (unsigned int row = 0; row < 3 && !free_cell; ++row){
-        for (unsigned int colum = 0; colum < 3 && !free_cell; ++colum){
+    for (unsigned int row = 0; row < N && !free_cell; ++row){
+        for (unsigned int colum = 0; colum < N && !free_cell; ++colum){
             free_cell = free_cell || (board[row][colum] == '-');
         }
     }
@@ -117,11 +119,12 @@ int main(void)
 {
     printf("TicTacToe: \n");
 
-    char board[3][3] = {
-        { '-', '-', '-' },
-        { '-', '-', '-' },
-        { '-', '-', '-' }
-    };
+    char board[N][N];
+    for (unsigned int i = 0; i < N; ++i){
+        for (unsigned int j = 0; j < N; ++j){
+            board[i][j] = '-';
+        }
+    }
 
     char turn = 'X';
     char winner = '-';
@@ -136,8 +139,8 @@ int main(void)
             exit(EXIT_FAILURE);
         }
         if (cell >= 0 && cell <= CELL_MAX) {
-            int row = cell / 3;
-            int colum = cell % 3;
+            int row = cell / N;
+            int colum = cell % N;
             if (board[row][colum] == '-') {
                 board[row][colum] = turn;
                 turn = turn == 'X' ? 'O' : 'X';
